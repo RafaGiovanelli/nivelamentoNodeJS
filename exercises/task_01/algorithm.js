0f7d680d953c25504cbce70840f1142fb39d9473
RafaGiovanelli <rsgiovanelli98@hotmail.com>
2024-04-19 21:41:35 -0300

    Feat: Criacao da task 01

--- exercises/task_01/algorithm.js
+++ exercises/task_01/algorithm.js
@@ -1,18 +1,34 @@
 //! Exercicio 1
 //* Implemente uma função que calcule a quantidade mínima de passos completos que pessoa levaria para chegar do ponto A ou B,
-//* sabendo que cada passo essa pessoa percorre uma distância fixa.
+//* sabendo que cada passo essa pessoa percorre tem uma distância fixa.
 
 //* Validações importantes!
-//* Validar se todas as entradas são numéricas.
-//* O número de passos deve ser o menor número inteiro para chegar ao destino.
-//* A distância de A não pode ser menor que B.
+//* Validar se todas as entradas são numéricas. ok
 //* A distância fixa por passo não pode ser negativa.
+//* A distância de A não pode ser menor que B. ok
+//* O número de passos deve ser o menor número inteiro para chegar ao destino.
 
-//? Ex: 5,40,2
+//? Ex: 40,5,2
 //? Resp: 18
 
-const task01 = () => {
-  //* Escreva sua solução aqui
+const task01 = (pontoA, pontoB, distanciaPassos) => {
+  pontoA = Number(pontoA);
+  pontoB = Number(pontoB);
+  distanciaPassos = Number(distanciaPassos);
+
+  if (typeof pontoA == "number" && typeof pontoB == "number" && typeof distanciaPassos == "number") {
+    if (distanciaPassos <= 0) {
+      console.log("Numero de passos não pode ser menor ou igual a 0!")
+    }
+    else if (pontoA < pontoB) {
+      console.log("A distancia do Ponto A não pode ser menor que a do Ponto B!")
+    }
+    else {
+
+      return Math.round((pontoA - pontoB) / distanciaPassos);
+
+    }
+  }
 };
 
-task01();
+console.log('Resultado :>> ', task01(process.argv[2], process.argv[3], process.argv[4]));
